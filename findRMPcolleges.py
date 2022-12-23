e9e89a43e9e9dbe28d9138c8cb0eadcfe8aebe33
nte0004 <evansnolan0004@gmail.com>
2022-12-22 18:18:07 -0600

    This program scrapes the ratemyprofessors.com schools to gather the school ID and name, to put into a csv files so that the csv file can be used later by the rmpwebscraper.py to work with every school.

--- findRMPcolleges.py
+++ findRMPcolleges.py
@@ -3,17 +3,30 @@ import json
 from bs4 import BeautifulSoup
 
 collegeList = []
-
 collegeNum = 1
 def findSchool(number):
     url = f'https://www.ratemyprofessors.com/school?sid={number}'
     r = requests.get(url)
-    soup = BeautifulSoup(r.content, 'html.parser')
-    script = soup.find_all('script')[11].text.strip()[25:-1061]
-    data = json.loads(script)
-    school = data['client:root'][2]['__ref']
-    print(school)
-while int(collegeNum) < 2 :
+    if r.status_code == 404:
+        errorMSG(r.status_code,number)
+    else:
+        soup = BeautifulSoup(r.content, 'html.parser')
+        script = soup.find_all('script')[11].text.strip()[25:-1061]
+        soup = BeautifulSoup(r.content, 'html.parser')
+        data = json.loads(script)
+        school = data['client:root']
+        id = list(school)[2][9:-2]
+        college = {
+            'School' : data[id]['name'],
+            'Legacy ID' : data[id]['legacyId'],
+            'sid' : data[id]['id']
+
+        }
+        collegeList.append(college)
+def errorMSG(status, num):
+    print(f'HTTPError: {status}, School {num} Does not exist.')
+
+while int(collegeNum) < 6050 :
     findSchool(collegeNum)
     collegeNum = int(collegeNum) + 1
-print(collegeList)
+print(json.dumps(collegeList, indent=4, sort_keys=False))
