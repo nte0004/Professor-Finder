5c4b1cd0490ba262de9a0a6dbb41a86a4f1a96e2
Nolan <evansnolan0004@gmail.com>
2022-12-26 22:22:24 -0600

    If input is an exact match it will not require a choice from the user anymore.

--- schoolSearch.py
+++ schoolSearch.py
@@ -1,31 +1,41 @@
 import pandas as pd
 
-def searchInput():
+file = 'schoolData.csv'
+
+def searchInput() -> str:
     school = input('Enter the name of your school: ')
     return school
 
-def searchFor(file, search):
+def searchFor(file: str, search: str) -> pd.DataFrame:
     df = pd.read_csv(file)
     df = df[df['School'].str.contains(search, case=False)]
-    return df
+    schoolList = list(df['School'])
+    if len(schoolList) == 0:
+        print('\nNo Results Found. Make sure spelling is correct and try again.\n')
+        main()
+    else:
+        possible_matches = df['School'].str.fullmatch(search, case=False)
+        for index, value in enumerate(possible_matches):
+            if value == True:
+                print(df.iloc[index])
+                return df.iloc[index]['sid']
+        choice = chooseSchool(schoolList)
+        print(df.iloc[int(choice)])
+        return df.iloc[int(choice)]['sid']
 
-def chooseSchool(options):
+def chooseSchool(options: list):
     print(f'Your search matched {len(options)} school(s):')
     for number, school in enumerate(options):
-        print(number, school)
+        print(f'{number} -- {school}','\n')
     choice = input('Enter the number of the school you would like to use (Enter "none" to search again): ')
     if choice == 'none':
         main()
     else:
-        return int(choice)
+        return choice
 
 def main():
-    file = 'schoolData.csv'
     search = searchInput()
-    results = searchFor(file, search)
-    options = list(results['School'])
-    choice = chooseSchool(options)
-    return results.iloc[choice]['sid']
+    return searchFor(file, search)
 
 if __name__ == '__main__':
     main()
