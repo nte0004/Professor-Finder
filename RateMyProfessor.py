cb392abd0624e92c08c2cba68efb33c1a252342d
Nolan <evansnolan0004@gmail.com>
2022-12-26 22:24:15 -0600

    formatting changes

--- RateMyProfessor.py
+++ RateMyProfessor.py
@@ -18,52 +18,52 @@ def requestNames():
 #   that name on the rate my professor webiste. The information needed is all in a script from the source code of
 #   the professor page. The JSON aspect of that script is isolated and then the function calls getProfessorInfo to
 #   further isolate the correct information.
-def lookupProfessor(firstName, lastName, sid):
-    pageUrl = f'https://www.ratemyprofessors.com/search/teachers?query={firstName}%20{lastName}&sid={sid}'
+def lookupProfessor(firstName: str, lastName: str, target_SID: str):
+    pageUrl = f'https://www.ratemyprofessors.com/search/teachers?query={firstName}%20{lastName}&sid={target_SID}'
     r = requests.get(pageUrl)
     soup = BeautifulSoup(r.content, 'html.parser')
     script = soup.find_all('script')[10].text.strip()[25:-1061]
-    getProfessorInfo(script, firstName, lastName, sid)
+    getProfessorInfo(script, firstName, lastName, target_SID)
 
 #getProfessorInfo creates and navigates a data dictionary with the script found with lookupProfessor.
 #   The script is converted to a JSON string then navigated in the usual dictionary methods. 
 #   Once all the information needed is found, it creates a Professor object and adds it to the PrefessorInfo list.
-def getProfessorInfo(JSON_String, first, last, sid):
+def getProfessorInfo(JSON_String: str, first: str, last: str, target_SID: str):
     data = json.loads(JSON_String)
     bs, open, close = '\"', '{\"', '\"}' # formatting variables to use f string in the refString
-    resultString = f'client:root:newSearch:teachers(after:{bs}{bs},first:8,query:{open}fallback{bs}:true,{bs}schoolID{bs}:{bs}{sid}{bs},{bs}text{bs}:{bs}{first} {last}{close})'
+    resultString = f'client:root:newSearch:teachers(after:{bs}{bs},first:8,query:{open}fallback{bs}:true,{bs}schoolID{bs}:{bs}{target_SID}{bs},{bs}text{bs}:{bs}{first} {last}{close})'
     resultCount = int(data[resultString]['resultCount'])
     if resultCount == 0:
-        profDNE(first, last, resultCount)
+        badResult(first, last, resultCount)
         return
-    resultEdges = data[resultString]['edges']['__refs']
+    professorList = data[resultString]['edges']['__refs']
 
-    for edge in resultEdges: #Each edge is essentially a different professor found in the query
-        profRef = data[edge]['node']['__ref']
-        tid = data[profRef]['legacyId']
-        profSchoolID = data[profRef]['school']['__ref']
-        schoolName = data[profSchoolID]['name']
+    for professor in professorList:
+        prof_ID = data[professor]['node']['__ref']
+        legacy_ID = data[prof_ID]['legacyId']
+        school_ID = data[prof_ID]['school']['__ref']
+        school_Name = data[school_ID]['name']
 
-        if profSchoolID != sid: #When the professor being looked at is not the right school
-            profDNE(first, last, resultCount)
+        if school_ID != target_SID:
+            badResult(first, last, resultCount)
             return
 
         else:
             professor = {
-                'First Name' : data[profRef]['firstName'],
-                'Last Name' : data[profRef]['lastName'],
-                'School' : schoolName,
-                'Department' : data[profRef]['department'],
-                'Rating' : data[profRef]['avgRating'],
-                'Difficulty' : data[profRef]['avgDifficulty'],
-                'Taken Again %' : str(data[profRef]['wouldTakeAgainPercent']).replace('-1', 'NA'),
-                'Reviews' : data[profRef]['numRatings'],
-                'Professor Page' : f'https://www.ratemyprofessors.com/professor?tid={tid}'
+                'First Name' : data[prof_ID]['firstName'],
+                'Last Name' : data[prof_ID]['lastName'],
+                'School' : school_Name,
+                'Department' : data[prof_ID]['department'],
+                'Rating' : data[prof_ID]['avgRating'],
+                'Difficulty' : data[prof_ID]['avgDifficulty'],
+                'Taken Again %' : str(data[prof_ID]['wouldTakeAgainPercent']).replace('-1', 'NA'),
+                'Reviews' : data[prof_ID]['numRatings'],
+                'Professor Page' : f'https://www.ratemyprofessors.com/professor?tid={legacy_ID}'
             }
             professorInfo.append(professor)
 
 #This function deals with instances where there are no results, or results not matching the correct school
-def profDNE(first, last, resultCount):
+def badResult(first: str, last: str, resultCount: int):
     if resultCount == 0:
         professor = {
             'First Name' : first,
@@ -80,10 +80,10 @@ def profDNE(first, last, resultCount):
         professorInfo.append(professor)
 
 def main():
-    sid = schoolSearch.main()
+    target_SchoolID = schoolSearch.main()
     requestNames()
     for name in professorNames:
-        lookupProfessor(name[0], name[1], sid)
+        lookupProfessor(name[0], name[1], target_SchoolID)
     print(json.dumps(professorInfo, indent=4, sort_keys=False))
     exit(0)
     
