d4870fbd33ea115eaed95b6340ac054ed38ebfb4
Nolan <evansnolan0004@gmail.com>
2022-12-28 22:42:08 -0600

    Removed unecessary functions, since input comes from GUI now. Simplified to one function to work with GUI middle man.

--- schoolSearch.py
+++ schoolSearch.py
@@ -1,42 +1,13 @@
 import pandas as pd
 
 file = 'schoolData.csv'
-
-def searchInput() -> str:
-    school = input('Enter the name of your school: ')
-    return school
-
-def searchFor(file: str, search: str) -> pd.DataFrame:
+def findMatch(search: str):
     df = pd.read_csv(file)
     df = df[df['School'].str.contains(search, case=False)]
+    possibleMatch = df['School'].str.fullmatch(search, case=False)
+    for index, value in enumerate(possibleMatch):
+        if value == True:
+            return str(df.iloc[index]['School']), str(df.iloc[index]['sid'])
     schoolList = list(df['School'])
-    if len(schoolList) == 0:
-        print('\nNo Results Found. Make sure spelling is correct and try again.\n')
-        main()
-    else:
-        possible_matches = df['School'].str.fullmatch(search, case=False)
-        for index, value in enumerate(possible_matches):
-            if value == True:
-                print(df.iloc[index])
-                return df.iloc[index]['sid']
-        choice = chooseSchool(schoolList)
-        print(df.iloc[int(choice)])
-        return df.iloc[int(choice)]['sid']
-
-def chooseSchool(options: list):
-    print(f'Your search matched {len(options)} school(s):')
-    for number, school in enumerate(options):
-        print(f'{number} -- {school}','\n')
-    choice = input('Enter the number of the school you would like to use (Enter "none" to search again): ')
-    if choice == 'none':
-        main()
-    else:
-        return choice
-
-def main():
-    search = searchInput()
-    return searchFor(file, search)
-
-if __name__ == '__main__':
-    main()
-    exit(0)
+    sidList = list(df['sid'])
+    return schoolList, sidList
